9b054820d1106918928613d40f54e7bead1cf1c7
Samuel <75617851+snchz46@users.noreply.github.com>
2025-11-29 12:51:11 +0100

    Update setup.py for package configuration changes

--- ros2_ws/src/cobraflex/setup.py
+++ ros2_ws/src/cobraflex/setup.py
@@ -1,26 +1,27 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'cobraflex'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Samuel',
+    maintainer='samuel',
     maintainer_email='sanchezmorenosamuel23@gmail.com',
     description='ROS 2 control nodes for the Cobra chassis.',
-    license='MIT',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cobraflex_node = cobraflex.cobraflex_node:main',
-            'lidar_avoidance_node = cobraflex.lidar_avoidance_node:main',
+            'cobraflex_cmdvel_driver = cobraflex.cobraflex_cmdvel_driver:main',
+            'lidar_avoidance_pid_node = cobraflex.lidar_avoidance_pid_node:main',
         ],
     },
 )
