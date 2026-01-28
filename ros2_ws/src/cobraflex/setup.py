35198261a9d2bd5399a80ed56f293607c6d61920
Samuel <75617851+snchz46@users.noreply.github.com>
2026-01-28 08:36:42 +0100

    Add files via upload

--- ros2_ws/src/cobraflex/setup.py
+++ ros2_ws/src/cobraflex/setup.py
@@ -12,11 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.xml')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),          
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),         
+        
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),     
     ],
 
     install_requires=[
@@ -33,6 +37,7 @@ setup(
         'console_scripts': [
             'cobraflex_ros_driver = cobraflex.cobraflex_ros_driver:main',
             'lidar_avoidance_pid_node = cobraflex.lidar_avoidance_pid_node:main',
+            'cobraflex_odom = cobraflex.cobraflex_odom:main',
         ],
     },
 )
